229008ac6da01df1367ebaad2793a71d8ca7d1de
Popov Evgeniy <tuzik87@inbox.ru>
2019-07-22 18:50:00 +0300

    Initial version

---
+++ magicswfshellcode.pas
@@ -0,0 +1,167 @@
+unit magicswfshellcode;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Dialogs,
+  ExtCtrls, StdCtrls, ComCtrls, LazUTF8;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    LabeledEdit1: TLabeledEdit;
+    OpenDialog1: TOpenDialog;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure LabeledEdit1Change(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var Form1: TForm1;
+function get_path(): string;
+function convert_file_name(source:string): string;
+function execute_program(executable:string;argument:string):Integer;
+procedure window_setup();
+procedure dialog_setup();
+procedure interface_setup();
+procedure common_setup();
+procedure language_setup();
+procedure setup();
+procedure compile_flash(target:string);
+
+implementation
+
+function get_path(): string;
+begin
+get_path:=ExtractFilePath(Application.ExeName);
+end;
+
+function convert_file_name(source:string): string;
+var target:string;
+begin
+target:=source;
+if Pos(' ',source)>0 then
+begin
+target:='"';
+target:=target+source+'"';
+end;
+convert_file_name:=target;
+end;
+
+function execute_program(executable:string;argument:string):Integer;
+var code:Integer;
+begin
+try
+code:=ExecuteProcess(UTF8ToWinCp(executable),UTF8ToWinCp(argument),[]);
+except
+On EOSError do code:=-1;
+end;
+execute_program:=code;
+end;
+
+procedure window_setup();
+begin
+ Application.Title:='Magic swf shell';
+ Form1.Caption:='Magic swf shell 0.1';
+ Form1.BorderStyle:=bsDialog;
+ Form1.Font.Name:=Screen.MenuFont.Name;
+ Form1.Font.Size:=14;
+end;
+
+procedure dialog_setup();
+begin
+Form1.OpenDialog1.FileName:='*.swf';
+Form1.OpenDialog1.DefaultExt:='*.swf';
+Form1.OpenDialog1.Filter:='Adobe flash movies|*.swf';
+end;
+
+procedure interface_setup();
+begin
+Form1.Button1.ShowHint:=False;
+Form1.Button2.ShowHint:=Form1.Button1.ShowHint;
+Form1.Button2.Enabled:=False;
+Form1.LabeledEdit1.Text:='';
+Form1.LabeledEdit1.LabelPosition:=lpLeft;
+Form1.LabeledEdit1.Enabled:=False;
+end;
+
+procedure common_setup();
+begin
+window_setup();
+interface_setup();
+dialog_setup();
+end;
+
+procedure language_setup();
+begin
+Form1.LabeledEdit1.EditLabel.Caption:='Target file';
+Form1.Button1.Caption:='Open';
+Form1.Button2.Caption:='Start';
+Form1.OpenDialog1.Title:='Open a Adobe flash movie';
+end;
+
+procedure setup();
+begin
+common_setup();
+language_setup();
+end;
+
+procedure compile_flash(target:string);
+var host,player,argument:string;
+var message:array[0..5] of string=('Operation was successfully complete','Cant open input file','Cant create output file','Cant allocate memory','Executable file of Flash Player Projector was corrupted','Flash movie was corrupted');
+var status:Integer;
+begin
+host:=get_path()+'magicswf.exe';
+player:=get_path()+'player.exe';
+argument:=convert_file_name(player)+' '+convert_file_name(target);
+status:=execute_program(host,argument);
+if status=-1 then
+begin
+ShowMessage('Can not execute a external program');
+end
+else
+begin
+ShowMessage(message[status]);
+end;
+
+end;
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+setup();
+end;
+
+procedure TForm1.LabeledEdit1Change(Sender: TObject);
+begin
+Form1.Button2.Enabled:=Form1.LabeledEdit1.Text<>'';
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+if Form1.OpenDialog1.Execute()=True then
+begin
+Form1.LabeledEdit1.Text:=Form1.OpenDialog1.FileName;
+end;
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+compile_flash(Form1.LabeledEdit1.Text);
+end;
+
+{$R *.lfm}
+
+end.
---
+++ readme.txt
@@ -0,0 +1,36 @@
+                                Magic swf shell
+
+                                  Version 0.1
+
+                         This software made by Popov Evgeniy Alekseyevich
+
+System requirement
+
+Windows 2000 or higher
+
+Program description
+
+This program is a graphic shell for Magic swf.
+
+Prepare for first start
+
+Do these steps before first start:
+1. Put executable file of Magic swf to Magic swf shell directory.
+2. Rename executable file of Flash Player Projector to player.exe. Put it to Magic swf shell directory.
+
+License
+
+This program distributed under GNU GENERAL PUBLIC LICENSE.
+
+Contact
+
+You can send me a letter to tuzik87@inbox.ru
+
+About program modification
+
+Source code is included in source.zip archive.
+It can be compiled under Lazarus 2.0.2 or higher.
+
+Change log
+
+0.1 - Initial version.
